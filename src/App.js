8e78e872a1c71199e851a15d9d123478196dc736
dahyeonbae <qock6563@gmail.com>
2022-04-01 17:21:53 +0900

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, { useState } from "react";
+// import ReactDOM from "react-dom";
 
-function App() {
+const useInput = (initialvalue, validator) => {
+  const [value, setValue] = useState(initialvalue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event; // event 값으로 value를 가져온다는 뜻입니다.
+    let keepGoing = true;
+    if (typeof validator === "function") keepGoing = validator(value); // T F
+    if (keepGoing) {
+      setValue(value); // 10자 이내일 때만 변경이 가능.
+    }
+  };
+  return { value, onChange };
+};
+
+export default function App() {
+  const checkLeng = (text) => text.length < 10;
+  const name = useInput("Ms.", checkLeng);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {/* <input placeholder={name.value} onChange={name.onChange} /> */}
+      <input placeholder="name" {...name} />
     </div>
   );
 }
-
-export default App;
