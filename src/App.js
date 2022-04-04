6bac30954f8fadff6dd525cecac606d181d39d7b
dahyeonbae <qock6563@gmail.com>
2022-04-04 21:01:14 +0900

    useTabs hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import "./styles.css";
 import React, { useState } from "react";
-// import ReactDOM from "react-dom";
 
-const useInput = (initialvalue, validator) => {
-  const [value, setValue] = useState(initialvalue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event; // event 값으로 value를 가져온다는 뜻입니다.
-    let keepGoing = true;
-    if (typeof validator === "function") keepGoing = validator(value); // T F
-    if (keepGoing) {
-      setValue(value); // 10자 이내일 때만 변경이 가능.
-    }
+const SampleData = [
+  {
+    tab: "Tab Number 0",
+    content: "Tab Content of number 0"
+  },
+  {
+    tab: "Tab Number 1",
+    content: "Tab Content of number 1"
+  }
+];
+
+const useTabs = (initialvalue, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) return; // 예외처리
+  const [currentIndex, setCurrentIndex] = useState(initialvalue);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-  return { value, onChange };
 };
 
 export default function App() {
-  const checkLeng = (text) => text.length < 10;
-  const name = useInput("Ms.", checkLeng);
-
+  const { currentItem, changeItem } = useTabs(0, SampleData);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      {/* <input placeholder={name.value} onChange={name.onChange} /> */}
-      <input placeholder="name" {...name} />
+      <h1> "This is useTabs Hooks! (Customed)" </h1>
+      {SampleData.map((value, index) => (
+        <button onClick={() => changeItem(index)}> {value.tab} </button>
+      ))}
+      <div> {currentItem.content} </div>
     </div>
   );
 }
