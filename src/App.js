6eec2885c50e317f79179e96ddfeab8b1c174176
dahyeonbae <qock6563@gmail.com>
2022-04-04 21:35:47 +0900

    useRef hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import "./styles.css";
 import React, { useEffect, useState } from "react";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const changeTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerHTML = title;
-  };
-  useEffect(changeTitle, [title]);
-  return setTitle; // 변경되는 부분을 리턴하기 위해
-};
-
 export default function App() {
-  const titleUptator = useTitle("Html Title Loading...");
-  setTimeout(() => titleUptator("Title Changed!"), 5000);
+  const refExample = useRef();
+  setTimeout(()=>refExample.current.focus(), 5000);
   return (
     <div className="App">
       <h1> "This is useTabs Hooks!" </h1>
+      <input ref={refExample} placeholder="5초 후 자동 focused"></input> 
     </div>
   );
 }
